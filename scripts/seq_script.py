5d67b1bcd1cf00ec64513444a356d6acea7485e0
phinna <hhh34@nau.edu>
2013-07-29 12:04:05 -0600

    removed files

--- scripts/seq_script.py
+++ scripts/seq_script.py
@@ -11,27 +11,41 @@ __maintainer__ = "Huanhua Huang"
 __email__ = "hhh34@nau.edu"
 __status__ = "Development"
 
-from seq_0719 import assign_seqID_to_seqs
+from classify import seperate_taxonomy_list
+from assign_taxonomy import assign_seqID_to_seqs
 from qcli import (parse_command_line_parameters, 
                   make_option)
 
+
+taxonomy_levels=['1','2','3','4','5','6','7']
+
 script_info = {}
-script_info['brief_description'] = "Match each sequence ID to its corresponding nucleotide sequence for each sub-taxonomy"
-script_info['script_description'] = "This script classifies seqID based on each sub-taxonomy. Then, the correspondng nucleotide sequence will be picked for each seqID. The output file will show the seqID and its nucleotide sequence in different sub-taxonomy file under its own taxonomy directory"
+script_info['brief_description'] = "Split taxonomy and match sequence ID to its corresponding nucleotide sequence at specific taxonomy level"
+script_info['script_description'] = "This script split taxonomy into different taxonomy levels and match sequence ID to its corresponding nucleotide sequence at specified taxonomy level. The output file shows the sequence ID and its nucleotide sequence at specific taxonomy level under its own taxonomy directory "
 script_info['script_usage'] = [\
-("Match destinctive sequence ID to its corresponding nucleotide sequence for each distinctive taxonomy in one file.",
-"Match destinctive sequence ID to its corresponding nucleotide sequence for each distinctive sub-taxonomy in one taxonomy file, which includes all sub-taxonomies found by scientists  and write the results to its sub-taxonomy file, which are under its corresponding taxonomic rank directory",
-"%prog -i c_taxonomy.txt -o IDseq"),
-("Match destinctive sequence ID to its corresponding nucleotide sequence for each distinctive taxonomy in two different files",
-"Match destinctive sequence ID to its corresponding nucleotide sequence for each distinctive sub-taxonomy in two taxonomy files, which inlcude all sub-taxonomies found by scientists and write the results to its sub-taxonomy file under its corresponding taxonomic rank directory",
-"%prog -i c_taxonomy.txt,f_taxonomy.txt -o IDseq"),
-("Match destinctive sequence ID to its corresponding nucleotide sequence for each distinctive sub-taxonomy in many files",
-"Match destinctive sequence ID to its corresponding nucleotide sequence for each distinctive sub-taxonomy in many taxonomy files, which include all sub-taxonomies found by scientists and write the results to its sub-taxonomy file under its corresponding taxonomic rank directory",
-"%prog -i \"*.txt\" -o IDseq")]
-script_info['output_description']= "The sequence ID and its nucleotide sequence for each sub-taxonomy is written to its specific sub-taxonomy file"
+("Split taxonomy and match destinctive sequence ID to its corresponding nucleotide sequence at specified taxonomy level in one file.",
+ "Split taxonomy in one taxonomy file and match destinctive sequence ID to its corresponding nucleotide sequence based on a fasta file, which corresponds to its taxonom  y file at specified taxonomy level, like phylum level.  Write the results to each t  axonomy file based on their characteristics",
+  "%prog -t ./gg_12_10_otus/taxonomy/61_otu_taxonomy.txt -f ./gg_12_10_otus/rep_set/61_otus.fasta -l 6_-o IDseq"),
+("Split taxonomy and match destinctive sequence ID to its corresponding nucleotide sequence at specified taxonomy level in two files.",
+ "Split taxonomy in two taxonomy files and match destinctive sequence ID to its corresponding nucleotide sequence based on two fasta files, which correspond to their taxonomy files  at specified taxonomy level, like phylum taxonomy level. Write the results to each taxonomy file based on their characteristics.",
+"%prog -t ./gg_12_10_otus/taxonomy/61_otu_taxonomy.txt,./gg_12_10_otus/taxonomy/64_otu_taxonomy.txt -f ./gg_12_10_otus/rep_set/61_otus.fasta,./gg_12_10_otus/rep_set/64_otus.fasta -o IDseq"),
+("Split taxonomy and match destinctive sequence ID to its corresponding nucleotide sequence at specified taxonomy level in more than two files.",
+"Split taxonomy in more than two taxonomy files and match destinctive sequence ID to its corresponding nucleotide sequence based on more than two fasta file, which correspond to their taxonomy file at specified taxonomy level, like phylum taxonomy level. Write the results to each taxonomy file based on their characteristics",
+"%prog -t \"./gg_12_10_otus/taxonomy/*.txt\" -f \"./gg_12_10_otus/rep_set/*.fasta\" -o IDseq")]
+script_info['output_description']= "The sequence ID and its nucleotide sequence at specified taxonomy level is written to its specific taxonomy file"
 script_info['required_options'] = [
- make_option('-i','--input_fps',type="existing_filepaths",help='Input taxonomy files containing seqID and its corresponding taxonomies'),
- make_option('-o','--output_dir',type="new_dirpath",help='the output directory containing classified taxonomy directories')
+ make_option('-t','--input_taxonomy_fps',type="existing_filepaths",
+             help='Input taxonomy files containing seqID and'+\
+                   'its corresponding taxonomies'),
+ make_option('-f','--input_fasta_fps',type="existing_filepaths",
+             help='Input fasta files, which correspond its taxonomy files'),
+ make_option('-l','--taxonomy_level',type="choice",
+             help='Split input files at the specific taxonomy level.'
+                  ' Valid taxonomy levels are:'+\
+                   ','.join(taxonomy_levels)+'[default: %default]',
+                   choices=taxonomy_levels, default='7'),
+ make_option('-o','--output_dir',type="new_dirpath",
+             help='the output directory containing classified taxonomy directories')
 ]
 script_info['version'] = __version__
 
@@ -41,11 +55,15 @@ def main():
     option_parser, opts, args =\
        parse_command_line_parameters(**script_info)
     
-    otu_files=['61_otus.fasta','64_otus.fasta','67_otus.fasta','70_otus.fasta',
-	       '73_otus.fasta','76_otus.fasta','79_otus.fasta','82_otus.fasta',                '85_otus.fasta','88_otus.fasta','91_otus.fasta','94_otus.fasta',
-               '97_otus.fasta']    
- 
-    assign_seqID_to_seqs(opts.input_fps,otu_files,opts.output_dir)
+    sub_taxonomy_list=[]
+    if opts.taxonomy_level=="":
+        sub_taxonomy=seperate_taxonomy_list(opts.input_taxonomy_fps,'7')
+        sub_taxonomy_list.append(sub_taxonomy)
+        assign_seqID_to_seqs(sub_taxonomy_list,opts.input_fasta_fps,opts.output_dir)
+    else:
+        sub_taxonomy=seperate_taxonomy_list(opts.input_taxonomy_fps,opts.taxonomy_level)
+        sub_taxonomy_list.append(sub_taxonomy)
+        assign_seqID_to_seqs(sub_taxonomy_list,opts.input_fasta_fps,opts.output_dir)
   
 
 if __name__ == "__main__":
