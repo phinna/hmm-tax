58eb748f251af6a77a944c1410dfdb25b0a15215
phinna <hhh34@nau.edu>
2013-08-14 16:26:51 -0600

    update the script

--- scripts/split_taxonomy.py
+++ scripts/split_taxonomy.py
@@ -21,17 +21,17 @@ from qcli import (parse_command_line_parameters,
 taxonomy_levels=['1','2','3','4','5','6','7']
 
 script_info = {}
-script_info['brief_description'] = "Split taxonomy and match sequence ID to its corresponding nucleotide sequence at specific taxonomy level"
-script_info['script_description'] = "This script split taxonomy into different taxonomy levels and match sequence ID to its corresponding nucleotide sequence at specified taxonomy level. The output file shows the sequence ID and its nucleotide sequence at specific taxonomy level under its own taxonomy directory "
+script_info['brief_description'] = "Split taxonomy by level and match the otu ID to the corresponding nucleotide sequence at each specific taxonomy level"
+script_info['script_description'] = "This script splits taxonomy into different taxonomy levels and matches the otu ID to its corresponding nucleotide sequence at a specified taxonomic level. The output file contains the sequence ID and its nucleotide sequence at a specific taxonomy level under its own taxonomy directory "
 script_info['script_usage'] = [\
-("Split taxonomy and match destinctive sequence ID to its corresponding nucleotide sequence at specified taxonomy level in one file.",
- "Split taxonomy in one taxonomy file and match destinctive sequence ID to its corresponding nucleotide sequence based on a fasta file, which corresponds to its taxonom  y file at specified taxonomy level, like phylum level.  Write the results to each t  axonomy file based on their characteristics",
+("Split taxonomy and match the destinct otu ID to its corresponding nucleotide sequence at a specified taxonomy level in one file.",
+ "Split taxonomy in a taxonomy file and match the destinct otu ID to its corresponding nucleotide sequence based on the fasta file, which corresponds to the taxonomy file at a specified taxonomy level, for instance: phylum.  Write the results to each taxonomy file based on each otu level",
   "%prog -t ./gg_12_10_otus/taxonomy/61_otu_taxonomy.txt -f ./gg_12_10_otus/rep_set/61_otus.fasta -l 6_-o IDseq"),
-("Split taxonomy and match destinctive sequence ID to its corresponding nucleotide sequence at specified taxonomy level in two files.",
- "Split taxonomy in two taxonomy files and match destinctive sequence ID to its corresponding nucleotide sequence based on two fasta files, which correspond to their taxonomy files  at specified taxonomy level, like phylum taxonomy level. Write the results to each taxonomy file based on their characteristics.",
+("Split taxonomy and match destinct otu ID to its corresponding nucleotide sequence at a specified taxonomy level in two files.",
+ "Split taxonomy in two taxonomy files and match destinct otu ID to its corresponding nucleotide sequence based on two fasta files, which correspond to the taxonomy files  at a specified taxonomy level, for instance: phylum. Write the results to each taxonomy file based on each otu level.",
 "%prog -t ./gg_12_10_otus/taxonomy/61_otu_taxonomy.txt,./gg_12_10_otus/taxonomy/64_otu_taxonomy.txt -f ./gg_12_10_otus/rep_set/61_otus.fasta,./gg_12_10_otus/rep_set/64_otus.fasta -o IDseq"),
-("Split taxonomy and match destinctive sequence ID to its corresponding nucleotide sequence at specified taxonomy level in more than two files.",
-"Split taxonomy in more than two taxonomy files and match destinctive sequence ID to its corresponding nucleotide sequence based on more than two fasta file, which correspond to their taxonomy file at specified taxonomy level, like phylum taxonomy level. Write the results to each taxonomy file based on their characteristics",
+("Split taxonomy and match destinct otu ID to its corresponding nucleotide sequence at a specified taxonomy level in more than two files.",
+"Split taxonomy in more than two taxonomy files and match destinct otu ID to its corresponding nucleotide sequence based on more than two fasta file, which correspond to their taxonomy file at a specified taxonomy level, for instaince:phylum. Write the results to each taxonomy file based on each otu level",
 "%prog -t \"./gg_12_10_otus/taxonomy/*.txt\" -f \"./gg_12_10_otus/rep_set/*.fasta\" -o IDseq")]
 script_info['output_description']= "The sequence ID and its nucleotide sequence at specified taxonomy level is written to its specific taxonomy file"
 script_info['required_options'] = [
@@ -40,11 +40,11 @@ script_info['required_options'] = [
                    'its corresponding taxonomies'),
  make_option('-f','--input_fasta_fps',type="existing_filepaths",
              help='Input fasta files, which correspond its taxonomy files'),
- make_option('-l','--taxonomy_level',type="choice",
+ make_option('-l','--taxonomy_level',type='int',#"choices",
              help='Split input files at the specific taxonomy level.'
                   ' Valid taxonomy levels are:'+\
                    ','.join(taxonomy_levels)+'[default: %default]',
-                   choices=taxonomy_levels, default='7'),
+                   default=7),#choices=taxonomy_levels, default='7'),
  make_option('-o','--output_dir',type="new_dirpath",
              help='the output directory containing classified taxonomy directories')
 ]
@@ -59,12 +59,12 @@ def main():
     sub_taxonomy_list=[]
     if opts.taxonomy_level=="":
 	mkdir(opts.output_dir,0755)
-        sub_taxonomy=split_taxonomy_list(opts.input_taxonomy_fps,'7',opts.output_dir)
+        sub_taxonomy=split_taxonomy_list(opts.input_taxonomy_fps,string(7),opts.output_dir)
         sub_taxonomy_list.append(sub_taxonomy)
         assign_seqID_to_seqs(taxon_list,opts.input_fasta_fps,opts.output_dir)
     else:
 	mkdir(opts.output_dir,0755)
-        sub_taxonomy=split_taxonomy_list(opts.input_taxonomy_fps,opts.taxonomy_level,opts.output_dir)
+        sub_taxonomy=split_taxonomy_list(opts.input_taxonomy_fps,string(opts.taxonomy_level),opts.output_dir)
         sub_taxonomy_list.append(sub_taxonomy)
         assign_seqID_to_seqs(sub_taxonomy_list,opts.input_fasta_fps,opts.output_dir)
   
